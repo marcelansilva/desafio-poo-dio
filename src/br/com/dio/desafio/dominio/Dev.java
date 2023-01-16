ab88c7b1f9dbceea390af2c0946bf99ef75186fe
Marcela Nascimento Silva <marcelamns.dev@gmail.com>
2023-01-16 18:21:24 -0300

    correção final.

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
-    private String name;
+    private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
